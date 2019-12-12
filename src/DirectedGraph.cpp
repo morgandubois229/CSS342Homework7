66ddb68cdf41e569367ac5a6ab76503793874939
Morgan Du Bois <morgan@Morgans-MacBook-Pro.local>
2019-12-11 22:37:04 -0800

    Initial commit

---
+++ src/DirectedGraph.cpp
@@ -0,0 +1,37 @@
+#include "DirectedGraph.h"
+
+DirectedGraph::DirectedGraph(Edge *edges, int size) {
+    for (int i = 0; i < size; i++) {
+        int src = edges[i].src, dest = edges[i].dest;
+        if (nodes.count(src) == 0) {
+            nodes[src] = new SingleLinkedList<int>();
+        }
+        nodes[src]->add(dest);
+        if (nodes.count(dest) == 0) {
+            nodes[dest] = new SingleLinkedList<int>();
+        }
+    }
+}
+
+DirectedGraph::~DirectedGraph() {
+    for (auto it = nodes.begin(); it != nodes.end(); it++) {
+        delete (*it).second;
+    }
+}
+
+void DirectedGraph::printGraph() {
+    for (auto it = nodes.begin(); it != nodes.end(); it++) {
+        printf("node(%d) has edges: ", (*it).first);
+        for (auto itl = (*it).second->begin(); itl != (*it).second->end(); itl++) {
+            printf("%d ", (*itl));
+        }
+        printf("\n");
+    }
+}
+
+vector<int> *DirectedGraph::topologySort() {
+	// homework
+	// the return here is a placeholder. replace with your own code 
+	return new vector<int>();
+}
+
---
+++ test/DirectedGraphSortTest.cpp
@@ -0,0 +1,60 @@
+#include "DirectedGraph.h"
+#include "gtest/gtest.h"
+
+TEST(topology_sort, print_test) {
+    Edge edges[] =
+            {
+                    // pair (x, y) represents edge from x to y
+                    { 0, 1 }, { 0, 3 }, { 1, 2 },
+                    { 1, 3 }, { 2, 4 }, {3, 4}
+            };
+
+    DirectedGraph graph(edges, sizeof(edges)/sizeof(edges[0]));
+    graph.printGraph();
+}
+
+TEST(topology_sort, test_5_nodes) {
+    Edge edges[] =
+            {
+                    // pair (x, y) represents edge from x to y
+                    { 0, 1 }, { 0, 3 }, { 1, 2 },
+                    { 1, 3 }, { 2, 4 }, {3, 4}
+            };
+
+    DirectedGraph graph(edges, sizeof(edges)/sizeof(edges[0]));
+    auto v = graph.topologySort();
+    vector<int> ans1{0, 1, 3, 2, 4,};
+    vector<int> ans2{0, 1, 2, 3, 4,};
+
+    ASSERT_TRUE((*v)==ans1 || (*v)==ans2);
+
+    delete v;
+}
+
+TEST(topology_sort, test_2_nodes) {
+    Edge edges[] =
+            {
+                    // pair (x, y) represents edge from x to y
+                    { 1, 0 }
+            };
+
+    DirectedGraph graph(edges, sizeof(edges)/sizeof(edges[0]));
+    auto v = graph.topologySort();
+    vector<int> ans{1, 0};
+
+    ASSERT_TRUE((*v)==ans);
+
+    delete v;
+}
+
+TEST(topology_sort, test_1_nodes) {
+    Edge edges[] = {};
+
+    DirectedGraph graph(edges, sizeof(edges)/sizeof(edges[0]));
+    auto v = graph.topologySort();
+
+    ASSERT_TRUE((*v).empty());
+
+    delete v;
+}
+
